124408d1efa5f2563a92f82868aeb213ee9f3aa0
yw3 <yw3@abc.com>
2022-07-13 09:00:12 +0800

    up b scripts

---
+++ author/202375.js
@@ -0,0 +1,1 @@
+var authorList=[{"author_name":" 基普·索恩","customer_url":"s3-xingjichuanyue","image_path":"/files/book/cover/66132/cover.jpg","leading_role":" 基普·索恩 (Kip Thorne)","id":66132,"book_name":"星际穿越"}]
---
+++ author/202376.js
@@ -0,0 +1,1 @@
+var authorList=[{"author_name":"丹尼尔·笛福","customer_url":"s3-lubinxunpiaoliuji","image_path":"/files/book/cover/66133/cover.jpg","leading_role":"[英] 丹尼尔·笛福","id":66133,"book_name":"鲁滨逊漂流记"}]
